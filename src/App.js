a09af7e67687e97abd5d86c6f17aa780f6f712c3
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-17 22:03:41 -0500

    setup initial call to API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Pokemon Application</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loading: false,
+            character: {}
+        }
+        // Bind syntax
+        // this.handleChange = this.handleChange.bind(this)
+    }
+
+    componentDidMount() {
+        this.setState({loading: true})
+        fetch("https://pokeapi.co/api/v2/pokemon/1")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    loading: false,
+                    character: data
+                })
+            })
+    }
+
+
+    render() {
+        const text = this.state.loading ? "Loading..." : this.state.character.name
+        return (
+            <div>
+                <p>{text}</p>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
