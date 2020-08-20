767951fb07bef8c3e005ff6718d337f1fe673abf
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-20 10:55:20 -0500

    updated button style

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -13,63 +13,57 @@ const customStyles = {
     }
 };
 
-function PokemonCard({ pokemon }) {
+function PokemonCard({pokemon}) {
     const [modalIsOpen, setIsOpen] = useState(false)
     const [pokeModal, setPokeModal] = useState(null)
     let [listOfFavorites, setListOfFavorites] = useState([])
-    let openModal = (pokeModalFromLoop)=>{
+    let openModal = (pokeModalFromLoop) => {
         setPokeModal(pokeModalFromLoop)
         console.log(pokeModalFromLoop)
         setIsOpen(true);
     }
 
-    let closeModal = ()=> {
+    let closeModal = () => {
         setIsOpen(false);
     }
-    useEffect(()=>{
+    useEffect(() => {
         const savedPokemon = JSON.parse(localStorage.getItem('favoritePokemon'))
-        if(savedPokemon)setListOfFavorites(savedPokemon)
-    },[])
-    useEffect(()=>{
-        localStorage.setItem('favoritePokemon',JSON.stringify(listOfFavorites))
+        if (savedPokemon) setListOfFavorites(savedPokemon)
+    }, [])
+    useEffect(() => {
+        localStorage.setItem('favoritePokemon', JSON.stringify(listOfFavorites))
     }, [listOfFavorites])
-    let saveFavoritePokemon = (favPokemon)=>{
+    let saveFavoritePokemon = (favPokemon) => {
         setListOfFavorites([favPokemon.name, ...listOfFavorites])
     }
     let newPokemon = pokemon.map((ele, ind) => {
-        return (
-            <div className="card col s12 m3 l3" style={{ "margin": "50px" }} key={ind}>
-                <div className="card-image">
-                    <img src={ele.sprites.front_default} alt="pokemon" />
+            return (
+                <div className="card col s12 m3 l3" style={{"margin": "50px"}} key={ind}>
+                    <div className="card-image">
+                        <img src={ele.sprites.front_default} alt="pokemon"/>
 
-                        <button className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>saveFavoritePokemon(ele)}>
+                        <button className="btn-floating halfway-fab waves-effect waves-light red"
+                                onClick={() => saveFavoritePokemon(ele)}>
                             <i className="fas fa-heart"/>
                         </button>
-                    <hr />
-                </div>
+                        <hr/>
+                    </div>
 
-                <div className="card-content">
-                    <a href="#">
+                    <div className="card-content center-align">
                         <span
 
                             className="card-title center-align"
-                            style={{ "fontSize": "1.75vw" }}>{ele.name}</span>
-                    </a>
-                    <span className="center align">Height: {ele.height}</span>
-                    <br />
-                    <span className="center align">ID: {ind + 1}</span>
-                    <br />
-                    <span className="center align">Weight: {ele.weight}</span>
-                    <br />
-                    <span className="center align">Weight: {ele.weight}</span>
-                    <br />
-                    <br />
-                    <button onClick={() => openModal(ele)}>View Pokemon</button>
-                    {/*<span className="center align"><a*/}
-                    {/*    href={`https://pokeapi.co/api/v2/pokemon/${ele.id}`}>Link</a></span>*/}
-                </div>
-            </div>)
-    }
+                            style={{"fontSize": "1.75vw"}}>{ele.name}</span>
+
+                        <span className="center align">ID: {ind + 1}</span>
+                        <br/>
+                        <br />
+                        <button className="btn" style={{"backgroundColor": "red", "fontWeight" : "bold"}} onClick={() => openModal(ele)}>View Pokemon</button>
+                        {/*<span className="center align"><a*/}
+                        {/*    href={`https://pokeapi.co/api/v2/pokemon/${ele.id}`}>Link</a></span>*/}
+                    </div>
+                </div>)
+        }
     )
     return (
         <div className="row text-center" key={pokemon.id}>
@@ -79,9 +73,16 @@ function PokemonCard({ pokemon }) {
                 style={customStyles}
             >
                 <div className="card-content">
-                    <img src={pokeModal ? pokeModal.sprites.front_default : ''} alt="pokemon" />
-                    <a href="#"><span className="card-title center-align" style={{ "fontSize": "1.75vw" }}>{pokeModal ? pokeModal.name : ''}</span>
+                    <img src={pokeModal ? pokeModal.sprites.front_default : ''} alt="pokemon"/>
+                    <a href="#"><span className="card-title center-align"
+                                      style={{"fontSize": "1.75vw"}}>{pokeModal ? pokeModal.name : ''}</span>
                     </a>
+                    <br/>
+                    <span className="center align">Height: {pokeModal ? pokeModal.weight : ""}</span>
+                    <br/>
+                    <span className="center align">Weight: {pokeModal ? pokeModal.weight : ""}</span>
+                    <br/>
+                    <br/>
                 </div>
                 <button onClick={closeModal}>Close</button>
             </Modal>
