2c2981ed8f186c5191e1a9ecee139b27f4f848a9
Moriah Humphries <moriah.humphries@gmail.com>
2020-08-25 15:03:33 -0500

    rendered list of new pokemon, set up path to link to ID

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,5 +1,9 @@
 import React, {useState, useEffect} from "react";
 import './../App.css';
+import {
+    BrowserRouter as Router,
+    Link
+} from "react-router-dom";
 
 const PokemonDetail = () => {
     useEffect(() => {
@@ -12,7 +16,7 @@ const PokemonDetail = () => {
 
     const getSinglePokemon = async () => {
         const data = await fetch(
-            "https://pokeapi.co/api/v2/pokemon"
+            "https://pokeapi.co/api/v2/pokemon?limit=151"
         );
         const items = await data.json();
         console.log(items.results);
@@ -20,11 +24,17 @@ const PokemonDetail = () => {
     }
 
     return (
-       <div>
-           {items.map(item => (
-           <h1 key={item.id}>{item.name}</h1>
-       ))}
-       </div>
+        <div>
+            {items.map((item, ind) => (
+                <h1 key={ind}>
+                    <Router>
+                        <Link to={`/single/${ind + 1}`} style={{"color": "white"}}>
+                            <p>{item.name}</p>
+                        </Link>
+                    </Router>
+                </h1>
+            ))}
+        </div>
     )
 }
 
